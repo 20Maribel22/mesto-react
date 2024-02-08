4cf54e720fba81fcf46292456444e61b6638bc60
Marina Belan <belanm77@gmail.com>
2024-02-08 12:26:58 +0600

    Updates

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export default {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__item',
-    submitButtonSelector: '.popup__button-save',
-    inactiveButtonClass: 'popup__button_inactive',
-    inputErrorClass: 'popup__item_type_error',
-    errorClass: 'popup__item-error_active'
-  };
